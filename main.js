8c40015183fa7b52602026fd521219f26eccfdc4
Kevin Luk <kanorikuo@gmail.com>
2020-08-05 06:41:32 +0900

    httpservice

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+var express = require('express')
 
+var server = express()
+
+server.get('/', function(request, response){
+    response.send('get response')
+})
+
+server.post('/', function(request, response){
+    response.send('post response')
+})
+
+server.listen(4040)
+console.log('service launched')
